2a7e6ed493f762b675c83215028681522802d5b2
karanOO89 <gk.987680@gmail.com>
2021-04-21 00:45:14 +0000

    Character count with red color functionality upon count above 140 added

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#tweet-text").keyup(function (event) {
+    //element
+    const counter = $(this).closest("form").find(".counter");
+    let color = $(this).closest("form").find(".counter");
+    let len = $(this).val().length;
+
+    let newCounter = 140 - len;
+    console.log(newCounter);
+
+    if (newCounter < 0) {
+      counter.addClass("counter-red");
+    }
+    counter.html(newCounter);
+  });
+});
